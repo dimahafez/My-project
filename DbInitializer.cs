d253e67f4f7efbaf37ec0ac75a1221572879c350
Dima Alhafez <111871337+dimahafez@users.noreply.github.com>
2024-04-04 18:21:59 +0200

    Add files via upload

---
+++ DbInitializer.cs
@@ -0,0 +1,60 @@
+﻿using LibraryManagement.Data.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data
+{
+    public static class DbInitializer
+    {
+        public static void Seed(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<LibraryDbContext>();
+
+
+                // Add Customers
+                var Dima = new Customer { Name = "Dima" };
+
+                var Hasan = new Customer { Name = "Hasan " };
+
+                var Loulia = new Customer { Name = "Loulia  " };
+
+                context.Customers.Add(Dima);
+                context.Customers.Add(Hasan);
+                context.Customers.Add(Loulia);
+
+                // Add Author
+                var authorDeMarco = new Author
+                {
+                    Name = "M J DeMarco",
+                    Books = new List<Book>()
+                {
+                    new Book { Title = "The Millionaire Fastlane" },
+                    new Book { Title = "Unscripted" }
+                }
+                };
+
+                var authorCardone = new Author
+                {
+                    Name = "Grant Cardone",
+                    Books = new List<Book>()
+                {
+                    new Book { Title = "The 10X Rule"},
+                    new Book { Title = "If You're Not First, You're Last"},
+                    new Book { Title = "Sell To Survive"}
+                }
+                };
+
+                context.Authors.Add(authorDeMarco);
+                context.Authors.Add(authorCardone);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
